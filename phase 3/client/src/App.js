f02f5312fde6467d39fefcd6502bc0f25618b84c
rahalyy <azizrahaly2004@gmail.com>
2025-07-16 02:28:19 +0200

    Add files via upload

---
+++ phase 3/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookSearchAndRecommend from './BookSearchAndRecommend';
+
+function App() {
+  return (
+    <div className="App">
+      <BookSearchAndRecommend />
+    </div>
+  );
+}
+
+export default App;
